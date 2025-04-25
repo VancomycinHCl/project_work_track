528df93947184b438a78856fc431ec87f0ae39d4
xuanzhi liu <2112484232@qq.com>
2025-04-25 10:51:11 +0300

    Add arrow calculation function and giving instance to draw arrow statically on a canvaswq

--- arrow.py
+++ arrow.py
@@ -1,27 +1,37 @@
 import cv2
 import numpy as np
 import math
+from typing import Tuple
 
-# 创建一张白底图
-img = np.ones((500, 500, 3), dtype=np.uint8) * 255
 
-# 起点
-x0, y0 = 250, 250
 
-# 设定箭头长度和角度
-length = 100
-theta_deg = 45  # 角度 (度)
-theta_rad = math.radians(theta_deg)  # 转为弧度
+def get_dest(length: int = 100, theta_deg: float=45.0) -> Tuple[int, int]:
+    x0: int = 0
+    y0: int = 0
+    theta_rad = math.radians(theta_deg)  # 转为弧度
+    x1 = int(x0 + length * math.cos(theta_rad))
+    y1 = int(y0 - length * math.sin(theta_rad)) # Attention! In openCV lib, the y axis is downward
+    return (x1,y1)
 
-# 计算终点坐标
-x1 = int(x0 + length * math.cos(theta_rad))
-y1 = int(y0 - length * math.sin(theta_rad))  # 注意OpenCV y轴向下
 
-# 画箭头
-cv2.arrowedLine(img, (x0, y0), (x1, y1), color=(0, 0, 255), thickness=2, tipLength=0.1)
+if __name__ == "__main__":
+    
 
-# 显示
-cv2.imshow('Arrow Example', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    img = np.ones((500, 500, 3), dtype=np.uint8) * 255
+
+    x0, y0 = 250, 250
+    x1, y1 = 0, 0
+
+    length = 100
+    theta_deg = 45  # degree 
+
+
+    (x1, y1) = get_dest(length, theta_deg)
+    x1 = x0 + x1
+    y1 = y0 + y1
+    cv2.arrowedLine(img, (x0, y0), (x1, y1), color=(0, 0, 255), thickness=2, tipLength=0.1)
+
+    cv2.imshow('Arrow Example', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
